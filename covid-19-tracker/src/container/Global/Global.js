cd26d1c161fd235bfbbf3d41c6e9873b033d2a8f
VidelRosendyDirgantara <videl2706@gmail.com>
2022-03-26 22:32:08 +0700

    sinkroniasi data dengan API realtime

--- covid-19-tracker/src/container/Global/Global.js
+++ covid-19-tracker/src/container/Global/Global.js
@@ -1,11 +1,43 @@
 import React, {Component} from 'react';
 import './Global.css';
+import axios from 'axios';
 
 //Import Component
 import WorldStats from '../../component/WorldStats/WorldStats';
 
 class Global extends Component {
+    state = {
+        result: {
+            "TotalConfirmed":0,
+            "TotalDeaths":0,
+            "TotalRecovered":0,
+            "ActiveCase":0
+        }
+    }
+
+    async componentDidMount() {
+        var globalData = await axios.get("https://api.covid19api.com/summary");
+
+        let corona = globalData.data.Global;
+
+        this.setState({
+            result: {
+                "TotalConfirmed":corona.TotalConfirmed,
+                "TotalDeaths": corona.TotalDeaths,
+                "TotalRecovered": corona.TotalConfirmed,
+                "ActiveCase": corona.TotalConfirmed - (corona.TotalDeaths + corona.TotalRecovered)
+            }
+        })
+    }
+    
     render() {
+        var Stats = Object.keys(this.state.result).map((key, index) => {
+            return <WorldStats
+                    key = {index}
+                    about = {key}
+                    total = {this.state.result[key]}/>
+        })
+
         return(
             <div className= "Global">
                 <h1 className = "heading">Covid-19 Tracker</h1>
@@ -13,20 +45,7 @@ class Global extends Component {
 
                 <div className = "world-stats">
 
-                    <WorldStats
-                    key="1"
-                    about="Total Case"
-                    total="255.123"/>
-
-                    <WorldStats
-                    key="1"
-                    about="Total Deaths"
-                    total="155.123"/>
-
-                    <WorldStats
-                    key="1"
-                    about="Total Recovered"
-                    total="175.123"/>
+                {Stats}
 
                 </div>
             </div>
